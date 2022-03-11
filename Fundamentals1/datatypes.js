781ef41567ac9ebfa5ec0fbe8de1f1d4136bd5bd
Marvin Abante <abante.marvin@yahoo.com>
2022-03-11 22:52:11 +0800

    Datatypes

---
+++ Fundamentals1/datatypes.js
@@ -0,0 +1,13 @@
+//Datatypes
+//decimals and integers.
+let age = 35;
+
+//String
+let firstName = "Marvin";
+
+//Boolean
+let fullAge = true;
+
+//Undefined means empty value, also null
+let children;
+
