15587828514fa9106ef610c933f656155b79a818
Marvin Abante <abante.marvin@yahoo.com>
2022-03-11 23:47:13 +0800

    operatorPrecedence.js made and done.

---
+++ Fundamentals1/operatorPrecdence.js
@@ -0,0 +1,14 @@
+/**
+ * Precendence operator
+ */
+
+let x, y; //Create variables without assignments.
+x = y = 25 - 10; //Now assign values to our valriables.
+console.log(x, y);
+
+let curyear = 2022;
+const ageMarvin = (curyear - 1986);
+const ageKay = (curyear - 1988);
+const averageAge = ((ageMarvin + ageKay) / 2);
+//Averaging
+console.log(averageAge);
