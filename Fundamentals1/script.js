49898f622c084e986a235d72d62d12ed4644d0df
Marvin Abante <abante.marvin@yahoo.com>
2022-03-11 21:07:34 +0800

    Added notes for reserved keywords and naming convention.

--- Fundamentals1/script.js
+++ Fundamentals1/script.js
@@ -6,5 +6,10 @@ console.log(40 + 8 + 23 - 10);
 console.log("Marvin");
 console.log(10);
 
+//Naming convention firstName
+//first_name
+// underscore and dollar sign is allowed in variable names _ $
+//reserved keywords sample are name and function.
+
 let firstName = "Marvin";
 console.log(firstName);
