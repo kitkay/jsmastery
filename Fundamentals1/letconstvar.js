59b05ff3fbef911baa6332f5193551e0be3a1139
Marvin Abante <abante.marvin@yahoo.com>
2022-03-11 23:07:29 +0800

    let var const - creating variables

---
+++ Fundamentals1/letconstvar.js
@@ -0,0 +1,15 @@
+/**
+ * let for variables that could change later.
+ * const - used to variables that cannot changed.
+ * var - works like let
+ *
+ * never use var as possible.
+ * immutable means unchanging over time or unable to be chagnged.
+ *  or immutable simply means it channot mutate.
+ */
+
+//Re-assigning or mutate the value
+let age = 30;
+age = 35;
+
+console.log(age);
