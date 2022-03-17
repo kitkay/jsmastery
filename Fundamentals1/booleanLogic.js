4b582e0bc79e5fbb66c1c0c2682d2d42c2fbeadc
Marvin Abante <abante.marvin@yahoo.com>
2022-03-18 01:44:51 +0800

    Basics of logical operator

---
+++ Fundamentals1/booleanLogic.js
@@ -0,0 +1,19 @@
+/**
+ * Boolean Logic - uses true or false to solve complex logical problems
+ *
+ * AND OR NOT
+ */
+
+const age = 16;
+
+/**
+ * A - False
+ * B - True
+ *
+ * A AND B = False
+ * A OR B = TRUE
+ * !A AND B = TRUE
+ * !A OR B = TRUE
+ * A AND !B = FALSE
+ * A OR !B = FALSE
+ */
